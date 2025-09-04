b0b89195f86d23709e0e192b9646045c039b9270
heiley wong <heileywongmss@gmail.com>
2025-09-04 09:19:59 +0100

    Delete project directory

--- project/tcl/cldensity.tcl
+++
@@ -1,5 +0,0 @@
-set cl [atomselect top "name CLA"]
-
-volmap density $cl -allframes -combine avg -res 1 -o efcldens.dx
-
-mol load dx efcldens.dx
--- project/tcl/nadensity.tcl
+++
@@ -1,5 +0,0 @@
-set na [atomselect top "name SOD"]
-
-volmap density $na -allframes -combine avg -res 1 -o efnadens.dx
-
-mol load dx efnadens.dx
